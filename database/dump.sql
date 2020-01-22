b9a8d486982e57e56f47b804824503e9143f24d2
Ethan Cordes <ethancordes7@gmail.com>
2020-01-22 10:43:48 -0800

    add eight cars to cars table in superCars db

--- database/dump.sql
+++ database/dump.sql
@@ -196,6 +196,14 @@ ALTER TABLE ONLY public.users ALTER COLUMN "userId" SET DEFAULT nextval('public.
 --
 
 COPY public.cars ("carId", make, image, year, "shortDescription", "topSpeed", "horsePower", rate, mpg, category, availability) FROM stdin;
+1	Porsche 918	/images/porsche-918.jpg	2015	The Porsche 918 Spyder is a mid-engine plug-in hybrid sports car manufactured by German automobile manufacturer Porsche.	214	887	2000	22	Hypercar	f
+2	Ferrari La Ferrari	/images/ferrari-la-ferrari.jpg	2016	LaFerrari, project name F150 (also unofficially referred to as the Ferrari LaFerrari or Ferrari F150), is a limited production hybrid sports car built by Italian automotive manufacturer Ferrari.	218	949	2000	13	Hypercar	f
+3	Pagani Huayra	/images/pagani-huayra.jpg	2017	The Pagani Huayra, it’s the successor to the almighty Pagani Zonda and it’s named after a South American wind god called Huayra Tata. 	210	700	1500	16	Supercar	f
+4	Bugatti Chiron	/images/bugatti-chiron.jpg	2018	The Bugatti Chiron is a mid-engine two-seater sports car developed and manufactured in Molsheim, France by French automobile manufacturer Bugatti Automobiles S.A.S..	261	1500	2500	10	Supercar	f
+5	Ford GT	/images/ford-gt.jpg	2019	The teardrop shape of the Ford GT is the result of extensive work in the wind tunnel. Its carbon-fiber body and its 647-horsepower 3.5L EcoBoost® V6 engine are the ultimate expressions of technological innovation.	216	647	1250	10	Supercar	f
+6	Ferrari 328 GTS	/images/ferrari-328-gts.jpg	1989	The Ferrari 328 GTB and GTS (Type F106) are mid-engine V8, two seat sports cars produced by Italian automotive manufacturer Ferrari.	166	260	650	14	Supercar	f
+7	Ford Mustang Shelby GT350	/images/ford-mustang-shelby-gt350.jpg	2019	As a track-ready version of the Ford Mustang, the 2019 Shelby GT350 has even more muscular styling and next-level performance.	180	710	500	16	Muscle	f
+8	Lamborghini Gallardo	/images/lamborghini-gallardo.jpg	2009	The Lamborghini Gallardo is a sports car built by the Italian automotive manufacturer Lamborghini from 2003 to 2013.	202	560	800	15	Supercar	f
 \.
 
 
