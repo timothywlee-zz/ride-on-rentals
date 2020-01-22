17d104c4539dd37e4ff98a87eea7136788a47942
AndrewRobins51413 <h.andrew.robins@gmail.com>
2020-01-22 23:23:03 +0000

    Robins - view details from one car - will need updating

--- client/components/user-view-details-front-end.jsx
+++ client/components/user-view-details-front-end.jsx
@@ -1,44 +1,45 @@
 import React from 'react';
 
 class CarDetailsList extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            carDetails:[],
-            isClicked: false
+  constructor(props) {
+    super(props);
+    this.state = {
+      carDetails: [],
+      isClicked: false
+    };
+    this.handeClick = this.handleClick.bind(this);
+  }
+
+  dataDidMount() {
+    fetch('/api/cars/')
+      .then(response => response.json())
+      .then(carDetailsData => {
+        this.setState({ carDetails: carDetailsData });
+      })
+      .catch(error => `${error}`);
+  }
+
+  handleClick() {
+    // return to main screen
+  }
+
+  render() {
+    return (
+      <div id="root">
+        <div className="container .container-fluid ">
+          <div className="row">
+            <button onClick={this.props.handleClick}></button>
+            {
+              this.state.carDetails.map(carDetail => (
+                <carRenderCode key={carDetail.carDetails} carDetailData={carDetail}>
+                </carRenderCode>
+              ))
             }
-        this.handeClick=this.handleClick.bind(this);
-        }
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
-        dataDidMount() {
-            fetch('/api/cars/')
-                .then(response => response.json())
-                .then(carDetailsData => {
-                    this.setState({ carDetails: carDetailsData });
-            })
-                .catch(error => `${error}`);
-        }
-        handleClick(){
-            //return to main screen
-        }
-        render() {
-            return (
-                <div id="root">
-                    <div className="container .container-fluid ">
-                        <div className="row">
-                        <button onClick={this.props.handleClick}></button>
-                            {
-                                this.state.carDetails.map(carDetail => (
-                                    <carRenderCode key={carDetail.carDetails} carDetailData={carDetail}>
-                                    </carRenderCode>
-                                ))
-                            }
-                        </div>
-                    </div>
-                </div>
-            );
-        }
-    }
-    
-    export default CarDetailList;
-
+export default CarDetailList;
