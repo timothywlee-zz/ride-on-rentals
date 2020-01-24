b705c5c21a286486077167089dd6af4668c5bb06
Ethan Cordes <ethancordes7@gmail.com>
2020-01-23 22:56:31 -0800

    add title, carousel placeholder, and button that links to car list

--- client/components/home-page.jsx
+++ client/components/home-page.jsx
@@ -1,19 +1,21 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link,
-//   Redirect
-// } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
     return (
       <div className="container-vertical">
-        <div className="home-title py-5">DISCOVER</div>
-        <div className="home-title">CAROUSEL</div>
-        <div className="home-title">VIEW CARS</div>
+        <div className="home-title">DISCOVER</div>
+        <div className="carousel">CAROUSEL</div>
+        <Link to="/cars">
+          <button id="view-cars">VIEW CARS</button>
+        </Link>
       </div>
     );
   }
